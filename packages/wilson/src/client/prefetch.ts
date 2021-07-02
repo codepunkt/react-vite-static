e3f0c37538c65829758af0669b4f26d7752d33d0
Christoph Werner <christoph+github@codepunkt.de>
2021-07-02 10:32:23 +0200

    feat: don't prefetch on slow connection
    - also use xhr if fetch is not available

--- packages/wilson/src/client/prefetch.ts
+++ packages/wilson/src/client/prefetch.ts
@@ -1,4 +1,6 @@
+import throttle from 'throttles'
 import { options, VNode } from 'preact'
+
 import { Dependencies } from '../types'
 
 declare global {
@@ -7,24 +9,83 @@ declare global {
   }
 }
 
+interface Navigator {
+  connection?: {
+    effectiveType: 'slow-2g' | '2g' | '3g' | '4g'
+    saveData: boolean
+  }
+}
+
+/**
+ * Fetches a given URL using XMLHttpRequest
+ */
+const viaXHR = (url: string): Promise<unknown> => {
+  return new Promise((res, rej) => {
+    const req = new XMLHttpRequest()
+    req.open(`GET`, url, (req.withCredentials = true))
+    req.onload = () => {
+      req.status === 200 ? res(req.status) : rej(req.status)
+    }
+    req.send()
+  })
+}
+
+/**
+ * Fetches a given URL using `<link rel=modulepreload>` or `<link rel=prefetch>`
+ */
+const viaDOM = (url: string): Promise<unknown> => {
+  return new Promise((res, rej) => {
+    const link = document.createElement('link')
+    const isScript = url.endsWith('.js')
+    link.rel = isScript ? 'modulepreload' : 'prefetch'
+    link.as = isScript ? 'script' : 'style'
+    link.crossOrigin = ''
+    link.href = url
+    link.onload = res
+    link.onerror = rej
+    document.head.appendChild(link)
+  })
+}
+
+const prefetch = (url: string) => {
+  const link = document.createElement('link')
+  const hasPrefetch =
+    link.relList && link.relList.supports && link.relList.supports('prefetch')
+  return hasPrefetch ? viaDOM(url) : viaXHR(url)
+}
+
+type PrefetchOptions = {
+  throttle?: number
+}
+
 /**
  * Enables prefetch of relative link resources on focus and mouseOver
  * by injecting modulepreload/preload links into the <head>
+ *
+ * @todo check which options from quicklink might be worthwhile to have
+ * and decide whether or not to differentiate between intersectionobserver,
+ * onFocus and onMouseOver prefetching
  */
-export const enablePrefetch = (): void => {
+export const enablePrefetch = (options: PrefetchOptions): void => {
+  const [toAdd, isDone] = throttle(3)
   const pathPreloads = (window.__WILSON_DATA__ ?? {}).pathPreloads
-  const findElements = (query: string): HTMLElement[] =>
-    Array.from(document.querySelectorAll(query))
-  const assets: Set<string> = new Set([
-    ...(findElements(
-      'head link[rel=stylesheet], head link[rel=preload], head link[rel=modulepreload]'
+
+  /**
+   * An array of relative script and style URLs
+   */
+  const assets = new Set(
+    Array.from(
+      document.querySelectorAll(
+        'head link[rel=stylesheet], head link[rel=preload], head link[rel=modulepreload], head script[src]'
+      )
     )
-      .map((element) => element.getAttribute('href'))
-      .filter(Boolean) as string[]),
-    ...(findElements('head script[src]')
-      .map((element) => element.getAttribute('src'))
-      .filter(Boolean) as string[]),
-  ])
+      .map((e) => {
+        return e.getAttribute(
+          e.tagName.toLowerCase() === 'script' ? 'src' : 'href'
+        )
+      })
+      .filter(Boolean)
+  )
 
   const vnodeHook = ({
     type,
@@ -43,17 +104,24 @@ export const enablePrefetch = (): void => {
         ...(pathPreloads[props.href!] ?? { css: [] }).css,
       ]
       preloads.forEach((href) => {
-        href = `/${href}`
-        if (!assets.has(href)) {
-          const tag = document.createElement('link')
-          const isScript = href.endsWith('.js')
-          tag.setAttribute('rel', isScript ? 'modulepreload' : 'preload')
-          tag.setAttribute('as', isScript ? 'script' : 'style')
-          tag.setAttribute('crossorigin', '')
-          tag.setAttribute('href', href)
-          document.head.appendChild(tag)
-          assets.add(href)
+        const url = `/${href}`
+
+        // Don't prefetch if using 2G or if saveData is enabled
+        const conn = (navigator as Navigator).connection
+        if (conn && (conn.saveData || /2g/.test(conn.effectiveType))) {
+          return
         }
+
+        // Don't prefetch if asset was already fetched or prefetched
+        if (assets.has(href)) {
+          return
+        }
+
+        assets.add(href)
+        toAdd(async () => {
+          await prefetch(url)
+          isDone()
+        })
       })
     }
 
@@ -70,6 +138,8 @@ export const enablePrefetch = (): void => {
     }
   }
 
+  // define VNode option hook
+  // @see https://preactjs.com/guide/v10/options/
   const old = options.vnode
   options.vnode = (vnode) => {
     if (old) old(vnode)
